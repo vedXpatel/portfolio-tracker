8d4ddfefc6e8174b1aa8ee7f47e41211545f239a
vedXpatel <ved.gp@somaiya.edu>
2023-08-24 16:52:46 +0530

    :book: : added upstox

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,18 @@
+'use client';
 export default function Dashboard () {
+
+    const upstoxLogin = async() => {
+        const authURL: any = process.env.UPSTOX_URL;
+        console.log(process.env.UPSTOX_URL);
+        console.log('inside login function');
+        const redirectToLogin = await fetch(authURL);
+    }
+
     return(
         <>
             <div className="container">
                 <h1 className="title">Title</h1>
+                <button id="upstox" onClick={upstoxLogin}>Upstox</button>
             </div>
         </>
     )
