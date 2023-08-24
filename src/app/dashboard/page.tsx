60a575a8e00c3bac4aa842686ff20bf43d7d2c83
vedXpatel <ved.gp@somaiya.edu>
2023-08-24 16:32:42 +0530

    :mega: : initializting project

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard () {
+    return(
+        <>
+            <div className="container">
+                <h1 className="title">Title</h1>
+            </div>
+        </>
+    )
+}
