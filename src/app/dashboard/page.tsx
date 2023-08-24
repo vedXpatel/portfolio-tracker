1f69d7aa3bbb1bee6679d684900d3c2c1b84ccef
vedXpatel <ved.gp@somaiya.edu>
2023-08-24 18:30:07 +0530

    :bug: : removing bugs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function Dashboard () {
             <div className="container">
                 <h1 className="title">Title</h1>
                 <button id="upstox">
-                    <Link href="https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id=a8b92bce-8025-4cef-9e4a-fafbe12345a0&redirect_uri=https://localhost:3000/dashboard">
+                    <Link href={JSON.stringify(process.env.NEXT_PUBLIC_UPSTOX_URL)}>
                         Upstox
                     </Link>
                 </button>
