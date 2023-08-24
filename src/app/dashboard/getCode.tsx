13120891c341bb6957509313b707bf79f91219dc
vedXpatel <ved.gp@somaiya.edu>
2023-08-24 18:28:18 +0530

    :bug: : updating code

---
+++ src/app/dashboard/getCode.tsx
@@ -0,0 +1,13 @@
+import {useSearchParams} from "next/navigation";
+import React, {useEffect} from "react";
+
+
+export default function getCode () {
+    const searchParams = useSearchParams();
+    useEffect(() => {
+        searchParams && console.log(searchParams.get('code'));
+    }, []);
+    return (
+
+    );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,20 @@
 'use client';
-export default function Dashboard () {
+import Link from 'next/link'
+import {useSearchParams} from 'next/navigation';
+import React, {useEffect} from 'react';
 
-    const upstoxLogin = async() => {
-        const authURL: any = process.env.UPSTOX_URL;
-        console.log(process.env.UPSTOX_URL);
-        console.log('inside login function');
-        const redirectToLogin = await fetch(authURL);
-    }
+import axios from 'axios';
+export default function Dashboard () {
 
     return(
         <>
             <div className="container">
                 <h1 className="title">Title</h1>
-                <button id="upstox" onClick={upstoxLogin}>Upstox</button>
+                <button id="upstox">
+                    <Link href="https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id=a8b92bce-8025-4cef-9e4a-fafbe12345a0&redirect_uri=https://localhost:3000/dashboard">
+                        Upstox
+                    </Link>
+                </button>
             </div>
         </>
     )
