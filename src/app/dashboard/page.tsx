dbb84a309181a085c75667ea6459654fd1cc1c97
vedXpatel <ved.gp@somaiya.edu>
2023-08-25 00:00:33 +0530

    :added upstox authentication

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,37 @@
 'use client';
-import Link from 'next/link'
+import Link from 'next/link';
 import {useSearchParams} from 'next/navigation';
-import React, {useEffect} from 'react';
+import React, {SetStateAction, useEffect, useState} from 'react';
 
 import axios from 'axios';
 export default function Dashboard () {
+
+    const [code, setCode] = useState<string>('');
+
+    const fetchData = async() => {
+        await axios.post('https://api-v2.upstox.com/login/authorization/token?code=' + code + '&client_id='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID+'&client_secret='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET+'&redirect_uri=https://localhost:3000/dashboard&grant_type=authorization_code')
+            .then((response) => {console.log(response)})
+            .catch((error) => console.log(error))
+    }
+    
+    const searchParams = useSearchParams();
+    useEffect(() => {
+        if(searchParams){
+            const params = JSON.stringify(searchParams.get('code'));
+            setCode(params);
+            console.log(`Code: ${code}`);
+            code && fetchData();
+        }
+    }, []);
 
     return(
         <>
             <div className="container">
                 <h1 className="title">Title</h1>
                 <button id="upstox">
-                    <Link href={JSON.stringify(process.env.NEXT_PUBLIC_UPSTOX_URL)}>
+                    <a href={process.env.NEXT_PUBLIC_UPSTOX_URL}>
                         Upstox
-                    </Link>
+                    </a>
                 </button>
             </div>
         </>
