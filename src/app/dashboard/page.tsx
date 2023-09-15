49a5cf05305172ce5a4a84c7b1efd31c6f4324f1
vedXpatel <ved.gp@somaiya.edu>
2023-09-15 11:46:53 +0530

    :bug: :minor changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Dashboard () {
     const [code, setCode] = useState<string>('');
 
     const fetchData = async() => {
-        await axios.post('https://api-v2.upstox.com/login/authorization/token?code=' + code + '&client_id='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID+'&client_secret='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET+'&redirect_uri=https://localhost:3000/dashboard&grant_type=authorization_code')
+        await axios.post('https://api-v2.upstox.com/login/authorization/token?code=' + code + '&client_id='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID+'&client_secret='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET+'&redirect_uri=https://localhost:3001/dashboard&grant_type=authorization_code')
             .then((response) => {console.log(response)})
             .catch((error) => console.log(error))
     }
