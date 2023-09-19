4b4ae431e551d58b747bf997367336b46268ff32
vedXpatel <ved.gp@somaiya.edu>
2023-09-19 19:26:47 +0530

    :mega: : removed 404 error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,23 +6,49 @@ import React, {SetStateAction, useEffect, useState} from 'react';
 import axios from 'axios';
 export default function Dashboard () {
 
-    const [code, setCode] = useState<string>('');
+    const [code, setCode] = useState<string | null>();
 
     const fetchData = async() => {
-        await axios.post('https://api-v2.upstox.com/login/authorization/token?code=' + code + '&client_id='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID+'&client_secret='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET+'&redirect_uri=https://localhost:3001/dashboard&grant_type=authorization_code')
-            .then((response) => {console.log(response)})
-            .catch((error) => console.log(error))
+        // await axios.post('https://api-v2.upstox.com/login/authorization/token?code=' + code + '&client_id='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID+'&client_secret='+process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET+'&redirect_uri=https://localhost:3000/dashboard&grant_type=authorization_code')
+        //     .then((response) => {console.log(response)})
+        //     .catch((error) => console.log(error))
+    try {
+
+        const response = await axios({
+            method: 'POST',
+            url: 'https://api-v2.upstox.com/login/authorization/token',
+            headers: {
+                'accept': 'application/json',
+                'Api-Version': '2.0',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin':'*',
+            },
+            data: {
+                code: JSON.stringify(code),
+                client_id: JSON.stringify(process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID),
+                client_secret: JSON.stringify(process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET),
+                redirect_uri: 'https://localhost:3000/dashboard',
+                grant_type: 'authorization_code',
+            },
+        });
+        console.log(response.data);
+    } catch(error) {
+        console.warn(error);
+    }
     }
     
     const searchParams = useSearchParams();
     useEffect(() => {
         if(searchParams){
-            const params = JSON.stringify(searchParams.get('code'));
-            setCode(params);
+            setCode(searchParams.get('code'));  //couldn't set code using useState
             console.log(`Code: ${code}`);
-            code && fetchData();
         }
     }, []);
+    
+    useEffect(() => {
+        code && fetchData();
+        console.log(`Code inside: ${code}`);
+    },[code, fetchData])
 
     return(
         <>
