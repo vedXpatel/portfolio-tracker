077f7e2aba70aa944750c3e4f20844bf74029cb5
vedXpatel <ved.gp@somaiya.edu>
2023-10-01 14:14:13 +0530

    :mega: : theme toggle added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,34 @@
 "use client";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import React, { SetStateAction, useEffect, useState } from "react";
+import axios from "axios";
+import { ModeToggle } from "@/components/navbar/theme-toggle";
+
+export default function dashboard() {
+  const [code, setCode] = useState<string | null>();
+  const [authToken, setAuthToken] = useState<string | null>("something");
+
+  const fetchData = async () => {
+    try {
+      await axios
+        .post("http://localhost:5000/proxy", {
+          code: code,
+          client_id: process.env.NEXT_PUBLIC_UPSTOX_CLIENT_ID,
+          client_secret: process.env.NEXT_PUBLIC_UPSTOX_CLIENT_SECRET,
+          redirect_uri: "https://localhost:3000/dashboard",
+          grant_type: "authorization_code",
+          "Access-Control-Allow-Headers": "*",
+        })
+        .then((response) => {
+          console.log(response.data.access_token);
+          response.data.access_token !== undefined &&
+            setAuthToken(response.data.access_token);
+        })
+        .catch((error) => {
+          alert(error.response.data.errors[0].message); 
+          console.warn(error)});
+    } catch (error) {
       console.warn(error);
     }
   };
@@ -29,11 +59,12 @@
   useEffect(() => {
     code && fetchData();
     console.log(`Code inside: ${code}`);
-  }, [code, fetchData]);"use client";
+  }, [code, fetchData]);
 
   return (
     <>
       <div className="container">
+        <ModeToggle/>
         <h1 className="title">Title</h1>
         <button id="upstox">
           <a href={process.env.NEXT_PUBLIC_UPSTOX_URL}>Upstox</a>
